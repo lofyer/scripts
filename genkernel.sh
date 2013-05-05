9b1128e6cf1f0acec8c1a92e7d3fc57742a26dfb
lofyer <root@rex.virtfan.net>
2013-05-06 02:33:22 +0800

    genkernel.sh: add makeopts

--- genkernel.sh
+++ genkernel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-genkernel --menuconfig --kernel-config="$PWD/.config" --kerneldir="$PWD" --no-clean --no-mrproper --ramdisk-modules  all
+genkernel --menuconfig --kernel-config="$PWD/.config" --kerneldir="$PWD" --no-clean --no-mrproper --ramdisk-modules --makeopts=-j3 all
