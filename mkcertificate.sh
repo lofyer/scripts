894230e5b8f582db05a147d47571f8af2d5a5b29
demo <demo@asd.virtfan.net>
2013-12-12 12:10:59 +0800

    update mkcertificate.sh

--- mkcertificate.sh
+++ mkcertificate.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+echo "*****Generating your KEY*****"
 openssl genrsa -des3 -out server.key 1024 
-openssl req -new -key server.key -out server.csr 
 cp server.key server.key.org 
 openssl rsa -in server.key.org -out server.key 
-openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt 
+echo "*****Generating your csr*****"
+openssl req -new -key server.key -out server.csr
+echo "*****Generating your root crt*****"
+openssl x509 -req -days 3650 -in server.csr -signkey server.key -out server.crt
+echo "*****Generating your site crt*****"
+openssl req -new -key server.key -out server.csr
+openssl x509 -md5 -days 1825 -req -CA server.crt -CAkey server.key -CAcreateserial -CAserial ca.srl -in server.csr -out server-site.crt
+echo "*****Generating your self crt*****"
+openssl req -new -x509 -days 3650 -key server.key -out server-self.crt
+
 cat server.crt server.key > certificate.pem
