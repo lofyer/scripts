5e7adf4ebb04864d4e4e66112cb5cea2b462b3e8
lofyer <lofyer@localhost.localdomain>
2014-01-21 11:29:57 +0800

    添加CentOS桥接设置以及Gentoo快速安装脚本

--- gentoo/quick-install.sh
+++ gentoo/quick-install.sh
@@ -1,2 +1,35 @@
 #!/bin/bash
 # This is gentoo quick install scripts, except fdisk
+mkdir /install
+mount /dev/sdb2 /install
+cd /install
+wget stage3
+tar xjpf stage3
+
+mkdir boot
+mount /dev/sdb1 /boot
+
+cd usr
+wget portage
+tar xjpf portage
+
+cd /install
+mount /dev/sdb5 home
+mount -o bind /dev dev
+mount -o bind /sys sys
+mount -o bind /proc proc
+cp -L /etc/resolv.conf etc/resolv.conf
+
+chroot /install
+
+cat > /install/setup.sh << EOF
+source /etc/profile
+echo "127.0.0.1 rex.lofyer.org rex localhost localhost.localdomain" > /etc/hosts
+echo 'HOSTNAME="rex"' > /etc/hostname
+cat >> /etc/portage/make.conf << EOF_MAKE
+GENTOO_MIRRORS=""
+FETCH_COMMAND=""
+RESUME_COMMAND=""
+LINGUAS="zh_CN"
+EOF_MAKE
+EOF
