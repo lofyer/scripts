7a0a7a7b1abd8b118301547395b1255d416e4880
lofyer <lofyer@localhost.localdomain>
2014-03-06 11:20:11 +0800

    增加对centos的兼容

--- multi-ssh-nowpasswd/addhost.sh
+++ multi-ssh-nowpasswd/addhost.sh
@@ -4,4 +4,4 @@ then
 	echo -e "Usage: addhost.sh"
 	exit
 fi
-cat ~/.ssh/id_rsa.pub | ssh $1 'cat >> .ssh/authorized_keys2'
+cat ~/.ssh/id_rsa.pub | ssh $1 'cat >> .ssh/authorized_keys2; chmod 700 .ssh; chmod 640 .ssh/authorized_keys2'
