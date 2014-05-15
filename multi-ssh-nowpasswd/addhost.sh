bf67bf6661ec89075001988214dec148e69daf5d
lofyer <lofyer@gmail.com>
2014-05-15 20:51:06 +0800

    update: add multi_ssh a warning.

--- multi-ssh-nowpasswd/addhost.sh
+++ multi-ssh-nowpasswd/addhost.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ $1 = "" ]]
 then
-	echo -e "Usage: addhost.sh"
+	echo -e "Make sure that selinux has been set to permissive or disabled.\nUsage: addhost.sh"
 	exit
 fi
 cat ~/.ssh/id_rsa.pub | ssh $1 'cat >> .ssh/authorized_keys2; chmod 700 .ssh; chmod 640 .ssh/authorized_keys2'
