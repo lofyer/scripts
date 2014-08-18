ef10f274c5708b3e4d19655c060b1ee8798577b1
lofyer <lofyer@gmail.com>
2014-08-18 16:48:51 +0800

    update bootvm

--- ovirt/bootvm.py
+++ ovirt/bootvm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import urllib2
 from xml.dom import minidom
+import subprocess
 
 engine="192.168.1.200"
 username="admin@internal"
@@ -26,16 +27,11 @@ def StartVm(vm_uuid):
 	global engine, username, password
 	url = "https://" + engine + "/api/vms/" + vm_uuid + "/start/"
 
-	passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
-	passman.add_password(None, url, username, password)
-	authhandler = urllib2.HTTPBasicAuthHandler(passman)
-	opener = urllib2.build_opener(authhandler)
-	urllib2.install_opener(opener)
-
 	req = urllib2.Request(url=url, data="<action />", headers={'Content-Type':'application/xml'})
+	cmd='curl -k -u %s:%s https://%s/api/vms/%s/start -X POST -d "<action />" -H "Content-type: application/xml"' % (username, password, engine, vm_uuid)
 	print vm_uuid
-	print "before req"
-	res = urllib2.urlopen(req).read()
+	subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+	#res = urllib2.urlopen(req).read()
 
 def ParseVms(xml):
 	global desktops
