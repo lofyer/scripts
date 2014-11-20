473cf18a530c944bb6b3987690e2ec541b4756c5
lofyer <lofyer@gmail.com>
2014-11-20 13:24:59 +0800

    fix ssh without password

--- multi-ssh-nowpasswd/addhost.sh
+++ multi-ssh-nowpasswd/addhost.sh
@@ -5,5 +5,5 @@ declare -a HOSTS=($(cat hosts.list))
 cnt=${#HOSTS[@]}
 
 for ((i=0;i<cnt;i++)); do
-	cat ~/.ssh/id_rsa.pub | ssh ${HOSTS[i]} 'mkdir cat >> ~/.ssh/authorized_keys2; chmod 700 ~/.ssh; chmod 640 ~/.ssh/authorized_keys2'
+	cat ~/.ssh/id_rsa.pub | ssh ${HOSTS[i]} 'mkdir .ssh; cat >> ~/.ssh/authorized_keys2; chmod 700 ~/.ssh; chmod 640 ~/.ssh/authorized_keys2'
 done
