4c7ec3e6ab465099576c2c20a2646a295f76409c
lofyer <lofyer@gmail.com>
2015-03-19 16:25:03 +0800

    openldap 添加删除用户脚本

---
+++ openldap/delldapuser.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+
+# Script to delete a login to ldap. 
+# Not recommended for anything beyound casual use. ;)
+# Author: Rilindo Foster
+# Contact:  rilindo.foster@monzell.com
+# Date: 09/25/2011
+
+if [ -z $1 ];  then
+        echo "delldapuser.sh <username>"
+            exit 1
+        fi
+
+USERNAME=$1
+
+ldapdelete -x -w $LDAPPASS -D "cn=root,dc=$DOMAIN,dc=$ORG" "uid=$USERNAME,ou=$UNIT,dc=$DOMAIN,dc=$ORG"
+
+ldapdelete -x -w $LDAPPASS -D "cn=root,dc=$DOMAIN,dc=$ORG" "cn=$USERNAME,ou=$GROUP,dc=$DOMAIN,dc=$ORG"
+
+
+if [ ! -e $HOMEDIR/$ARCHIVE ]; then
+   echo "Creating archive directory"
+   mkdir -p $HOMEDIR/$ARCHIVE
+fi 
+mv -v $HOMEDIR/$USERNAME $HOMEDIR/$ARCHIVE/$USERNAME.$MODDATE
