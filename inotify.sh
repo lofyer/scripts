e177c539214176fb97dcf992a127fccfdaea7363
lofyer <lofyer@gmail.com>
2015-05-11 13:51:43 +0800

    change inotify to close_write

--- inotify.sh
+++ inotify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-inotifywait -mr --timefmt '%d/%m/%y %H:%M' --format '%T %w %f' -e create,modify,attrib /tmp/iso-uploads | while read date time dir file; do
+inotifywait -mr --timefmt '%d/%m/%y %H:%M' --format '%T %w %f' -e close_write /tmp/iso-uploads | while read date time dir file; do
     echo `date`
 done
