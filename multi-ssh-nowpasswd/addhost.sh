fcca4127086da1f7e4ed642b9ec9872f93c0dc1c
lofyer <lofyer@gmail.com>
2015-10-14 13:21:39 +0800

    update to ssh-copy-id

--- multi-ssh-nowpasswd/addhost.sh
+++ multi-ssh-nowpasswd/addhost.sh
@@ -5,5 +5,6 @@ declare -a HOSTS=($(cat hosts.list))
 cnt=${#HOSTS[@]}
 
 for ((i=0;i<cnt;i++)); do
-	cat ~/.ssh/id_rsa.pub | ssh ${HOSTS[i]} 'mkdir .ssh; cat >> ~/.ssh/authorized_keys2; chmod 700 ~/.ssh; chmod 640 ~/.ssh/authorized_keys2'
+	#cat ~/.ssh/id_rsa.pub | ssh ${HOSTS[i]} 'mkdir .ssh; cat >> ~/.ssh/authorized_keys2; chmod 700 ~/.ssh; chmod 640 ~/.ssh/authorized_keys2'
+	ssh-copy-id ${HOSTS[i]}
 done
