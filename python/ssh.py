5beff5425b63bef429d72670846991b01e054e2c
lofyer <lofyer@gmail.com>
2015-10-14 14:29:23 +0800

    add sftp to ssh.py

--- python/ssh.py
+++ python/ssh.py
@@ -6,3 +6,19 @@ ssh.connect("lofyer.org",22,"root", "password")
 stdin, stdout, stderr = ssh.exec_command("ls /")
 print stdout.readlines()
 ssh.close()
+
+t = paramiko.Transport((“主机”,”端口”))
+t.connect(username = “用户名”, password = “口令”)
+sftp = paramiko.SFTPClient.from_transport(t)
+remotepath=’/var/log/system.log’
+localpath=’/tmp/system.log’
+sftp.get(remotepath, localpath)
+t.close()
+
+t = paramiko.Transport((“主机”,”端口”))
+t.connect(username = “用户名”, password = “口令”)
+sftp = paramiko.SFTPClient.from_transport(t)
+remotepath=’/var/log/system.log’
+localpath=’/tmp/system.log’
+sftp.put(localpath,remotepath)
+t.close()
