dbbf64b47658b808258ff6369561c33da6e289f4
lofyer <lofyer@gmail.com>
2015-12-29 11:02:25 +0800

    add igxbe

---
+++ ovirt/hooks/ext_xml.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+"""
+Tweak an interface defintion so that its source is forced to be a specified
+network. Applied on a per vnic basis, it gets triggered and used by two
+different events:
+    * before_device_create
+    * before_nic_hotplug
+
+This hook can be used to force a VM to use a libvirt network that is managed
+outside of ovirt, such as an openvswitch network, or libvirt's default network.
+"""
+
+
+import os
+import sys
+import traceback
+from xml.dom import minidom
+
+import hooking
+
+#ext_string="""
+#<interface type='hostdev'>
+#  <mac address='9a:eb:fd:22:c4:67'/>
+#  <driver name='vfio'/>
+#  <source>
+#    <address type='pci' domain='0x0000' bus='0x04' slot='0x10' function='0x2'/>
+#  </source>
+#</interface>
+#"""
+
+ext_string="""
+<hostdev mode='subsystem' type='pci' managed='yes'>
+      <driver name='vfio'/>
+      <source>
+        <address domain='0x0000' bus='0x04' slot='0x10' function='0x2'/>
+      </source>
+      <alias name='igbxe'/>
+      <address type='pci' domain='0x0000' bus='0x00' slot='0x08' function='0x0' multifunction='on'/>
+</hostdev>
+"""
+
+def main():
+    #newnet = os.environ.get('extnet')
+    doc = hooking.read_domxml()
+    devices_dom = doc.getElementsByTagName("devices")[0]
+    ext_dom = minidom.parseString(ext_string)
+    ext_elements = ext_dom.getElementsByTagName("hostdev")[0]
+    devices_dom.appendChild(ext_elements)
+    hooking.write_domxml(doc)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        hooking.exit_hook('ext_xml hook: [unexpected error]: %s\n' %
+                          traceback.format_exc())
