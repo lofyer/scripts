7852d650984199cbf07003de9b52ce8573db9f15
lofyer <lofyer@gmail.com>
2016-03-17 23:56:26 +0800

    update

--- python/ssh.py
+++ python/ssh.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#! --* coding: utf-8 *--
 import paramiko
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -7,18 +8,18 @@ stdin, stdout, stderr = ssh.exec_command("ls /")
 print stdout.readlines()
 ssh.close()
 
-t = paramiko.Transport((“主机”,”端口”))
-t.connect(username = “用户名”, password = “口令”)
+t = paramiko.Transport(("主机","端口"))
+t.connect(username = "用户名", password = "口令")
 sftp = paramiko.SFTPClient.from_transport(t)
-remotepath=’/var/log/system.log’
-localpath=’/tmp/system.log’
+remotepath='/var/log/system.log'
+localpath='/tmp/system.log'
 sftp.get(remotepath, localpath)
 t.close()
 
-t = paramiko.Transport((“主机”,”端口”))
-t.connect(username = “用户名”, password = “口令”)
+t = paramiko.Transport(("主机","端口"))
+t.connect(username = "用户名", password = "口令")
 sftp = paramiko.SFTPClient.from_transport(t)
-remotepath=’/var/log/system.log’
-localpath=’/tmp/system.log’
+remotepath='/var/log/system.log'
+localpath='/tmp/system.log'
 sftp.put(localpath,remotepath)
 t.close()
