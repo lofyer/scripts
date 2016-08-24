6e9fd04cd6209459745f83dc8b303494992b2544
lofyer <lofyer@gmail.com>
2016-08-24 23:37:06 +0800

    snapshot

---
+++ ovirt/create_snapshot_by_tag.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import sys
+from ovirtsdk.api import API
+from datetime import date
+
+USERNAME = 'admin@internal'
+PASSWORD = 'admin'
+URL = 'https://localhost/api'
+SNAPSHOT_NAME = 'backup-%s' % date.today().strftime("%Y%m%d")
+
+api = API(URL, username=USERNAME, password=PASSWORD, insecure=True)
+
+vms = api.vms.list()
+
+for i in vms:
+    if i.tags.list() != [] and i.tags.list()[0].name == sys.argv[1] :
+        i.snapshots.add(params.Snapshot(description=SNAPSHOT_NAME, vm=i))
