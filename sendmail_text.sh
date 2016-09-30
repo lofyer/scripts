ab5bb03200b822663118d12cf05a72f999c02908
lofyer <lofyer@gmail.com>
2016-09-30 10:03:45 +0800

    update

--- sendmail_text.sh
+++ sendmail_text.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SUBJECT="Subject here"
 FROM="lofyer@gmail.com"
-TO="lofyer@gmail.com"
+TO="jiangdi007@163.com"
 DATE=$(date)
 MSG="The word you gonna say."
 mail="subject:$SUBJECT\nfrom:$FROM\n$MSG\n$DATE"
