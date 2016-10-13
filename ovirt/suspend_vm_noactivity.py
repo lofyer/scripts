98be861c125336c93e5fee1cfb4461cd56c8b19a
lofyer <lofyer@gmail.com>
2016-10-13 09:36:53 +0800

    suspend

--- ovirt/suspend_vm_noactivity.py
+++ ovirt/suspend_vm_noactivity.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#-*- coding: utf-8 -*-
 from ovirtsdk.api import API
 import time
 
@@ -7,20 +8,21 @@ PASSWORD = 'admin'
 URL = 'https://localhost/api'
 
 while True:
-    api = API(URL, username=USERNAME, password=PASSWORD, insecure=True)
-    vms = api.vms.list()
-    print vms[0].name
-    tagged_running_vms = [vm for vm in vms if vm.tags.list() != [] and vm.get_start_time() != None and vm.tags.list()[0].name == "FPGA"]
-    print tagged_running_vms
-    session_list = [ vm.sessions.list()[0].get_console_user() != None for vm in tagged_running_vms ]
-    time.sleep(600)
-    session_list_later = [ vm.sessions.list()[0].get_console_user() != None for vm in tagged_running_vms ]
-    if len(session_list) != len(session_list_later):
-        continue
-    for i in range(len(session_list_later)):
-        if session_list_later[i] == False and (session_list[i] ^ session_list_later[i]) == False:
-            try:
+    try:
+        api = API(URL, username=USERNAME, password=PASSWORD, insecure=True)
+        vms = api.vms.list()
+        print vms[0].name
+        tagged_running_vms = [vm for vm in vms if vm.tags.list() != [] and vm.get_start_time() != None and vm.tags.list()[0].name == "1130teach"]
+        print tagged_running_vms
+        session_list = [ vm.sessions.list()[0].get_ip() != None for vm in tagged_running_vms ]
+        time.sleep(600)
+        session_list_later = [ vm.sessions.list()[0].get_ip() != None for vm in tagged_running_vms ]
+        if len(session_list) != len(session_list_later):
+            continue
+        for i in range(len(session_list_later)):
+            if session_list_later[i] == False and (session_list[i] ^ session_list_later[i]) == False:
                 print "Suspend " + tagged_running_vms[i].name
                 tagged_running_vms[i].suspend()
-            except Exception as e:
-                print e
+    except Exception as e:
+        print e
+        pass
