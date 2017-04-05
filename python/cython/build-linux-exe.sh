913e003270b3e7d327efe04e0402acc6686b9b74
lofyer <lofyer@gmail.com>
2017-04-05 23:05:59 +0800

    update

--- python/cython/build-linux-exe.sh
+++ python/cython/build-linux-exe.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cython -3 --embed -o hello.c hello.py
-gcc -Os -I /usr/include/python3.3m -o hello hello.c -lpython3.3m -lpthread -lm -lutil -ldl
+cython -2 --embed -o hello.c hello.py
+gcc hello.c -framework Python
+#gcc -Os -I /usr/include/python3.3m -o hello hello.c -lpython3.3m -lpthread -lm -lutil -ldl
