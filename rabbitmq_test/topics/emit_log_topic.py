be95e38cc531272e7fc11fa304a56fa8378ffe8f
lofyer <lofyer@gmail.com>
2017-11-15 00:45:56 +0800

    update

--- rabbitmq_test/topics/emit_log_topic.py
+++ rabbitmq_test/topics/emit_log_topic.py
@@ -3,7 +3,7 @@ import pika
 import sys
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
+        host='35.194.187.175'))
 channel = connection.channel()
 
 channel.exchange_declare(exchange='topic_logs',
