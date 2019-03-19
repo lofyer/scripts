a2140af395de23cae0487101935f333289a6461a
lofyer <lofyer@lofyerdeMacBook-Pro.local>
2019-03-19 16:35:31 +0800

    update

--- python/graphviz-demo.py
+++ python/graphviz-demo.py
@@ -1,7 +1,7 @@
 from graphviz import Digraph
 dot = Digraph(comment='这是一个有向图')
 
-dot.node('A', '作者')
+dot.node('A', '医生', shape="box", image="asd.jpeg")
 dot.node('B', '医生')
 dot.node('C', '护士')
 
@@ -12,4 +12,4 @@ dot.format = 'png'
 dot.render('output-graph.gv', view=True)
 
 from IPython.display import display, Image
-Image(dot.render('output-graph.gv'))
+Image(dot.render('output-graph.png', format="png"))
