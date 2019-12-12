e255fb15ad14a9a989a733519855fd147ab07468
lofyer <lofyer@gmail.com>
2019-12-12 10:33:10 +0800

    update

---
+++ macos/iso2dmg3dd.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# diskutil unmountDisk /dev/disk2
+# hdiutil convert -format UDRW -o linux.dmg linux.iso
+# dd if=linux.dmg of=/dev/rdisk2 bs=10m
--- mysql/2-grant.sql
+++ mysql/2-grant.sql
@@ -1,2 +1,3 @@
+ALTER USER 'root'@'localhost' IDENTIFIED BY '123456';
 grant all privileges on *.* to root@'%' identified by '123456';
 flush privileges;
