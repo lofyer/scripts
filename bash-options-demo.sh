a4f9e2c06e1ec91f49ff01e50230965beef33d4d
lofyer <lofyer@gmail.com>
2022-04-13 22:29:01 +0800

    update

--- bash-options-demo.sh
+++ bash-options-demo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
 cat >/tmp/demo-space-separated.sh <<'EOF'
 #!/bin/bash
 
@@ -42,6 +43,74 @@ if [[ -n $1 ]]; then
     echo "Last line of file specified as non-opt/last argument:"
     tail -1 "$1"
 fi
+EOF
+
+
+cat >/tmp/demo-equals-separated.sh <<'EOF'
+#!/bin/bash
+
+for i in "$@"; do
+  case $i in
+    -e=*|--extension=*)
+      EXTENSION="${i#*=}"
+      shift # past argument=value
+      ;;
+    -s=*|--searchpath=*)
+      SEARCHPATH="${i#*=}"
+      shift # past argument=value
+      ;;
+    --default)
+      DEFAULT=YES
+      shift # past argument with no value
+      ;;
+    -*|--*)
+      echo "Unknown option $i"
+      exit 1
+      ;;
+    *)
+      ;;
+  esac
+done
+
+echo "FILE EXTENSION  = ${EXTENSION}"
+echo "SEARCH PATH     = ${SEARCHPATH}"
+echo "DEFAULT         = ${DEFAULT}"
+echo "Number files in SEARCH PATH with EXTENSION:" $(ls -1 "${SEARCHPATH}"/*."${EXTENSION}" | wc -l)
+
+if [[ -n $1 ]]; then
+    echo "Last line of file specified as non-opt/last argument:"
+    tail -1 $1
+fi
+EOF
+
+cat >/tmp/demo-getopts.sh <<'EOF'
+#!/bin/sh
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+output_file=""
+verbose=0
+
+while getopts "h?vf:" opt; do
+  case "$opt" in
+    h|\?)
+      show_help
+      exit 0
+      ;;
+    v)  verbose=1
+      ;;
+    f)  output_file=$OPTARG
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
+
+echo "verbose=$verbose, output_file='$output_file', Leftovers: $@"
 EOF
 
 chmod +x /tmp/demo-space-separated.sh
